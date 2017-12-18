50f6631c0a92bf128f4e1e04200f4ae9a1668190
Qwer Asder <peaser@ukr.net>
2017-12-18 16:30:09 +0200

    Realized bean definition inheritance

--- src/main/java/com/tutorialspoint/HelloWorld.java
+++ src/main/java/com/tutorialspoint/HelloWorld.java
@@ -1,20 +1,22 @@
 package com.tutorialspoint;
 
 public class HelloWorld {
-    private String message;
+    private String message1;
+    private String message2;
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-    public void getMessage() {
-        System.out.println("Your message : " + message);
+    public void setMessage1(String message) {
+        this.message1 = message;
     }
 
-    public void init() {
-        System.out.println("Bean is going through init.");
+    public void setMessage2(String message) {
+        this.message2 = message;
     }
 
-    public void destroy() {
-        System.out.println("Bean will destroy now.");
+    public void getMessage1() {
+        System.out.println("Your message : " + message1);
+    }
+
+    public void getMessage2() {
+        System.out.println("Your message : " + message2);
     }
 }
